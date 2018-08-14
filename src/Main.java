6833fd3413d4056c79920164dd55676f41f9d896
diabolique79 <m.mielnicka79@gmail.com>
2018-08-14 13:42:42 +0200

    12.2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Queue<Integer> list = new PriorityQueue<>();
+        Queue<Integer> list = new LinkedList<>();
         Scanner scan = new Scanner(System.in);
 
         int i = 0;
         int sum = 0;
         Integer number = 0;
-        while (i < 5) {
+        System.out.println("Wprowadź 10 wartości liczbowych unikalnych: ");
+        while (i < 10) {
 
             number = scan.nextInt();
             i++;
@@ -23,10 +21,13 @@ public class Main {
         }
 
         for (Integer lista : list) {
+            if(((LinkedList<Integer>) list).getLast()==lista){
 
+                System.out.print(lista + " = ");
+            }
+            else
             System.out.printf("%s + ", lista);
         }
-        System.out.println("");
         System.out.println(sum);
 
     }
